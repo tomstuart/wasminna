ed308e614c3754cb76f97d83b57b5c4b5d03783c
Tom Stuart <hi@tomstu.art>
2022-09-30 20:54:47 +0100

    Add support for parsing lists

--- wasm.rb
+++ wasm.rb
@@ -4,6 +4,11 @@ def main
   actual = SExpressionParser.new.parse(input)
   raise actual.inspect unless actual == expected
 
+  input = '(hello)'
+  expected = ['hello']
+  actual = SExpressionParser.new.parse(input)
+  raise actual.inspect unless actual == expected
+
   unless ARGV.empty?
     s_expression = SExpressionParser.new.parse(ARGF.read)
     pp s_expression
@@ -13,9 +18,32 @@ end
 class SExpressionParser
   def parse(string)
     self.string = string
+    parse_expression
+  end
+
+  def parse_expression
+    if can_read? %r{\(}
+      expressions = []
+      read %r{\(}
+      expressions << parse_expression
+      read %r{\)}
+      expressions
+    else
+      read %r{[^)]+}
+    end
   end
 
   private
+
+  def can_read?(pattern)
+    %r{\A#{pattern}}.match?(string)
+  end
+
+  def read(pattern)
+    match = %r{\A#{pattern}}.match(string) or raise "couldn’t match #{pattern} in #{string.inspect}"
+    self.string = match.post_match
+    match.to_s
+  end
 
   attr_accessor :string
 end
