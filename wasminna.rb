982ad5044b9fa5539d7f0f33901d35172b70cebe
Tom Stuart <hi@tomstu.art>
2022-10-05 22:22:20 +0100

    Implement the `i32.div_u` instruction

--- wasminna.rb
+++ wasminna.rb
@@ -85,6 +85,8 @@ class Interpreter
         signed_right = signed(evaluate(right, locals:), bits:)
         signed_result = divide(signed_left, signed_right)
         unsigned(signed_result, bits:)
+      in ['div_u', left, right]
+        divide(evaluate(left, locals:), evaluate(right, locals:))
       end.then { |value| mask(value, bits:) }
     end
   end
