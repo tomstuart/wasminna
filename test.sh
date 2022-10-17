0c2f6f47a1385b8f26097b5beac6032786ae75f0
Tom Stuart <hi@tomstu.art>
2022-10-17 21:40:58 +0100

    Use script’s directory as WASMINNA_PATH in test script

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-ruby -Iwasminna wasminna/s_expression_parser_test.rb
+WASMINNA_PATH=$(dirname "$0")
+
+ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/s_expression_parser_test.rb
 
 for script in int_literals i32 i64 int_exprs float_literals
 do
-  ruby -Iwasminna wasminna/wasminna.rb spec/test/core/$script.wast
+  ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb spec/test/core/$script.wast
 done
