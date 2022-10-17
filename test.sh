affe6b9f66c3dcd71f56a6921f09ceed22bd1c92
Tom Stuart <hi@tomstu.art>
2022-10-17 21:42:01 +0100

    Use WASM_SPEC_PATH to find WebAssembly specs in test script

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/s_expression_parser_test.rb
 
 for script in int_literals i32 i64 int_exprs float_literals
 do
-  ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb spec/test/core/$script.wast
+  ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$script.wast
 done
