62c46de65b84f374de80ba6b0255f54145327acc
Tom Stuart <hi@tomstu.art>
2022-10-19 00:39:23 +0100

    Add conversions.wast to the test script

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ WASMINNA_PATH=$(dirname "$0")
 
 ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/s_expression_parser_test.rb
 
-for script in int_literals i32 i64 int_exprs float_literals
+for script in int_literals i32 i64 int_exprs float_literals conversions
 do
   ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$script.wast
 done
