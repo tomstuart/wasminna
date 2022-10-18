d6b1b885361e4496bbb6e8ef11aa4e93989cba08
Tom Stuart <hi@tomstu.art>
2022-10-18 22:22:09 +0100

    Add test for encoding 0x7fffffff as a float
    This was previously triggering the bug of rounding a significand above
    the acceptable maximum.

--- float_test.rb
+++ float_test.rb
@@ -4,6 +4,7 @@ def main
   assert_float_encoding 0x4049_0fdb, 3141592653589793, 10 ** 15, bits: 32
   assert_float_encoding 0x5eff_ffff, 0x7fffff4000000001, 1, bits: 32
   assert_float_encoding 0x0000_0000, 0, 1, bits: 32
+  assert_float_encoding 0x4f00_0000, 0x7fffffff, 1, bits: 32
 end
 
 def assert_float_encoding(expected, numerator, denominator, negated: false, bits:)
