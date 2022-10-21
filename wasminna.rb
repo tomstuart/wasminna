e873c6b5e9c43ac7cc97b2404b22b066d1f763bd
Tom Stuart <hi@tomstu.art>
2022-10-21 23:17:19 +0100

    Implement the `f32.mul` instruction

--- wasminna.rb
+++ wasminna.rb
@@ -275,6 +275,10 @@ class Interpreter
         with_float(left, right, format:) do |left, right|
           left - right
         end
+      in ['mul', left, right]
+        with_float(left, right, format:) do |left, right|
+          left * right
+        end
       in ['convert_i32_s' | 'convert_i64_s', value]
         integer_bits = operation.slice(%r{\d+}).to_i(10)
         integer = signed(value, bits: integer_bits)
