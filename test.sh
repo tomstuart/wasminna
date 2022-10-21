4b9550f28d006cc36a61deb589339713d441cd40
Tom Stuart <hi@tomstu.art>
2022-10-21 22:37:58 +0100

    Run pending tests in test script
    The idea here is to make the next test script part of the CI run, so
    that I’ve got a good record of how far through it I am, and also a
    mechanism to alert me if I accidentally get everything passing without
    realising it.

--- test.sh
+++ test.sh
@@ -13,3 +13,13 @@ for script in int_literals i32 i64 int_exprs float_literals conversions
 do
   ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$script.wast
 done
+
+for pending in f32
+do
+  if ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$pending.wast; then
+    echo "error: pending test passed"
+    exit 1
+  else
+    echo "pending test failed successfully"
+  fi
+done
