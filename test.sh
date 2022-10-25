cb6277e76cb0c8b9de4a5c4e6f25877f0aeef7b1
Tom Stuart <hi@tomstu.art>
2022-10-25 22:51:01 +0100

    Implement the `f32.ge` instruction

--- test.sh
+++ test.sh
@@ -9,12 +9,12 @@ do
   ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/$test.rb
 done
 
-for script in int_literals i32 i64 int_exprs float_literals conversions f32 f32_bitwise
+for script in int_literals i32 i64 int_exprs float_literals conversions f32 f32_bitwise f32_cmp
 do
   ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$script.wast
 done
 
-for pending in f32_cmp
+for pending in
 do
   if ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$pending.wast; then
     echo "error: pending test passed"
