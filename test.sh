3573ca37cf14b27cb6e279842c594719a6261d82
Tom Stuart <hi@tomstu.art>
2022-10-25 23:43:31 +0100

    Turn off -x in test script and print our own information instead

--- test.sh
+++ test.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 WASMINNA_PATH=$(dirname "$0")
 
 for test in s_expression_parser_test float_test
 do
-  ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/$test.rb
+  file=$test.rb
+  printf "\e[1m%s\e[0m: " "$file"
+  ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/$file
 done
 
 for script in int_literals i32 i64 int_exprs float_literals conversions f32 f32_bitwise f32_cmp f64 f64_bitwise f64_cmp
 do
-  ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$script.wast
+  file=$script.wast
+  printf "\e[1m%s\e[0m: " "$file"
+  ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$file
 done
 
 for pending in float_exprs
 do
-  if ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$pending.wast; then
+  file=$pending.wast
+  printf "\e[1m%s\e[0m (pending): " "$file"
+  if ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$file; then
     printf "\e[31merror: pending test passed\e[0m\n"
     exit 1
   else
