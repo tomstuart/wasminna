d3ef75b1b8d50dff9404244b81583667d47e7412
Tom Stuart <hi@tomstu.art>
2022-10-27 00:05:18 +0100

    Implement the `f64.load` instruction

--- wasminna.rb
+++ wasminna.rb
@@ -442,8 +442,14 @@ class Interpreter
         raise unless bits == integer_bits
         value
       in ['load', value]
-        raise unless bits == 32
-        @memory.bytes.unpack1('L<', offset: value)
+        format =
+          case bits
+          in 32
+            'L<'
+          in 64
+            'Q<'
+          end
+        @memory.bytes.unpack1(format, offset: value)
       in ['store', offset, value]
         raise unless bits == 32
         @memory.bytes.force_encoding(Encoding::ASCII_8BIT)
