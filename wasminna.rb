df9da7bd252fe40199f44654c01ed6c01a1d4111
Tom Stuart <hi@tomstu.art>
2022-10-26 23:25:00 +0100

    Implement Interpreter#parse_string to interpret WebAssembly string syntax
    Strings are UTF-8 and contain (among other, as-yet-unhandled escape
    sequences) backslash-escaped two-digit hex characters [0] representing
    specific bytes. To unescape these, we convert the string to ASCII-8BIT
    encoding, then replace all the backslash sequences with their equivalent
    bytes, then convert back to UTF-8 so that untouched parts of the string
    can still be interpreted correctly.
    
    Unparsed strings are also the raw atoms from the S-expression, so they
    still have the surrounding quotes, which we must remove ourselves.
    
    [0] https://webassembly.github.io/spec/core/text/values.html#strings

--- wasminna.rb
+++ wasminna.rb
@@ -101,6 +101,16 @@ class Interpreter
     end
   end
 
+  def parse_string(string)
+    string.
+      delete_prefix('"').delete_suffix('"').
+      force_encoding(Encoding::ASCII_8BIT).
+      gsub!(%r{\\\h{2}}) do |digits|
+        digits.delete_prefix('\\').to_i(16).chr
+      end.
+      force_encoding(Encoding::UTF_8)
+  end
+
   def evaluate(expression, locals:)
     case expression
     in ['return', return_expression]
