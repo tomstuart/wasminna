5235ba1acb3ef33275416a31b7e5473937991ea3
Tom Stuart <hi@tomstu.art>
2022-10-26 20:51:54 +0100

    Use #assert_float_encoding in float unit test
    This is essentially what the previous code (which I wrote in anger while
    trying to debug the problem) was doing anyway, so it’s much clearer to
    use the existing assertion.

--- float_test.rb
+++ float_test.rb
@@ -8,22 +8,10 @@ def main
   assert_float_encoding 0x4f00_0000, 0x7fffffffr, bits: 32
 
   assert_float_encoding 0x3ff0_0000_0000_0000, 1r, bits: 64
+  assert_float_encoding 0x4337_e2c4_4058_a799, 6723256985364377r, bits: 64
 
   assert_float_decoding Wasminna::Float::Zero.new(sign: Sign::PLUS), 0x0000_0000, bits: 32
   assert_float_decoding Wasminna::Float::Finite.new(rational: 1r / 2 ** 149), 0x0000_0001, bits: 32
-
-  integer = 6723256985364377
-  format = Wasminna::Float::Format::Double
-  expected = [integer].pack('D').unpack1('Q')
-  actual = Wasminna::Float.from_integer(integer).encode(format:)
-  if actual == expected
-    print "\e[32m.\e[0m"
-  else
-    puts
-    puts "\e[31mFAILED\e[0m: \e[33mWasminna::Float#encode\e[0m"
-    puts "expected #{expected.inspect}, got #{actual.inspect}"
-    exit 1
-  end
 
   puts
 end
