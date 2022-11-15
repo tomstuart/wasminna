091ca165cb2869d014c60368e17f958b7f3cdc74
Tom Stuart <hi@tomstu.art>
2022-11-15 20:23:45 +0000

    Stop disabling experimental feature warnings in test script
    I’d disabled these in a7cae30cfb6f4d031b96dc8edde2d68c3bc7b6bb because
    of all the warnings about find patterns being experimental. Now that
    we’ve upgraded to Ruby 3.2.0, these warnings no longer exist so we don’t
    need to suppress them any more.

--- test.sh
+++ test.sh
@@ -8,21 +8,21 @@ for test in s_expression_parser_test float_test
 do
   file=$test.rb
   printf "\e[1m%s\e[0m: " "$file"
-  ruby -W:no-experimental -I"$WASMINNA_PATH" "$WASMINNA_PATH"/$file
+  ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/$file
 done
 
 for script in int_literals i32 i64 int_exprs float_literals conversions f32 f32_bitwise f32_cmp f64 f64_bitwise f64_cmp float_memory float_exprs float_misc const
 do
   file=$script.wast
   printf "\e[1m%s\e[0m: " "$file"
-  ruby -W:no-experimental -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$file
+  ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$file
 done
 
 for pending in
 do
   file=$pending.wast
   printf "\e[1m%s\e[0m (pending): " "$file"
-  if ruby -W:no-experimental -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$file; then
+  if ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$file; then
     printf "\e[31merror: pending test passed\e[0m\n"
     exit 1
   else
