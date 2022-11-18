c556be7d0c5645c13d8c630a5f41bb046a65aa84
Tom Stuart <hi@tomstu.art>
2022-11-18 23:07:48 +0000

    Unfold the `i64.load8_s`, `i32.store8` and `i64.store16` instructions
    These are the only three we need to make br.wast pass, so for now I’m
    avoiding the larger problem of how to support all possible storage sizes
    and sign extension modes [0].
    
    [0] https://webassembly.github.io/spec/core/syntax/instructions.html#memory-instructions

--- ast_parser.rb
+++ ast_parser.rb
@@ -20,7 +20,7 @@ class ASTParser
     case s_expression
     in ['i32.const' | 'i64.const' | 'f32.const' | 'f64.const' | 'local.get' | 'local.set' | 'local.tee' | 'global.set' | 'br' | 'br_if' | 'call' => opcode, argument, *rest]
       [*rest.flat_map { unfold(_1) }, opcode, argument]
-    in ['i32.load' | 'i64.load' | 'f32.load' | 'f64.load' | 'i32.store' | 'i64.store' | 'f32.store' | 'f64.store' => opcode, %r{\Aoffset=\d+\z} => static_offset, *rest]
+    in ['i32.load' | 'i64.load' | 'i64.load8_s' | 'f32.load' | 'f64.load' | 'i32.store' | 'i32.store8' | 'i64.store' | 'i64.store16' | 'f32.store' | 'f64.store' => opcode, %r{\Aoffset=\d+\z} => static_offset, *rest]
       [*rest.flat_map { unfold(_1) }, opcode, static_offset]
     in ['block' | 'loop' | 'if' => opcode, *rest]
       rest in [%r{\A\$} => label, *rest]
