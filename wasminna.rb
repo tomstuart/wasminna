a37f83306568e96fe92b935c1d6448aabe5b6bc8
Tom Stuart <hi@tomstu.art>
2022-12-02 01:44:44 +0000

    Replace #with_branch_handler `jump:` argument with optional `redo_on_branch:`
    @zetter used an optional boolean argument in #2 and I really like that
    idea, not least because it means we only need to provide the extra
    argument in the one case where we want `redo` to happen. He called the
    argument `branch_to_sequence_start:` in his PR, but I’ve gone with
    `redo_on_branch:` because it’s the most simple-minded name I could
    possibly think of, which means it’s more likely I’ll remember what it
    does in future.

--- wasminna.rb
+++ wasminna.rb
@@ -177,7 +177,7 @@ class Interpreter
 
     with_current_function(function) do
       catch(:return) do
-        with_branch_handler(label: nil, arity: type.results.length, jump: :forwards) do
+        with_branch_handler(label: nil, arity: type.results.length) do
           evaluate_expression(function.body, locals:)
         end
       end
@@ -251,18 +251,18 @@ class Interpreter
     in Drop
       stack.pop(1)
     in Block(label:, results:, body:)
-      with_branch_handler(label:, arity: results.length, jump: :forwards) do
+      with_branch_handler(label:, arity: results.length) do
         evaluate_expression(body, locals:)
       end
     in Loop(label:, results:, body:)
-      with_branch_handler(label:, arity: results.length, jump: :backwards) do
+      with_branch_handler(label:, arity: results.length, redo_on_branch: true) do
         evaluate_expression(body, locals:)
       end
     in If(label:, results:, consequent:, alternative:)
       stack.pop(1) => [condition]
       body = condition.zero? ? alternative : consequent
 
-      with_branch_handler(label:, arity: results.length, jump: :forwards) do
+      with_branch_handler(label:, arity: results.length) do
         evaluate_expression(body, locals:)
       end
     in BrTable(target_indexes:, default_index:)
@@ -281,7 +281,7 @@ class Interpreter
     end
   end
 
-  def with_branch_handler(label:, arity:, jump:)
+  def with_branch_handler(label:, arity:, redo_on_branch: false)
     tap do
       stack_height = stack.length
       result =
@@ -297,7 +297,7 @@ class Interpreter
           stack.pop(arity) => saved_operands
           stack.pop(stack.length - stack_height)
           stack.push(*saved_operands)
-          redo if jump == :backwards
+          redo if redo_on_branch
         else
           throw(:branch, result)
         end
@@ -306,7 +306,7 @@ class Interpreter
           stack.pop(arity) => saved_operands
           stack.pop(stack.length - stack_height)
           stack.push(*saved_operands)
-          redo if jump == :backwards
+          redo if redo_on_branch
         else
           throw(:branch, result - 1)
         end
