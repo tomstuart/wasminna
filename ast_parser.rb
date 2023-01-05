517fac719e0e59817dde316b85fd59f82d1845bf
Tom Stuart <hi@tomstu.art>
2023-01-05 00:34:56 +0000

    Use #gsub instead of #gsub! in #parse_string
    The latter method returns `nil` if no substitutions occur, which makes
    me confused about how this was ever working. Maybe we’ve never tried to
    parse a string literal that doesn’t contain any hex escapes before?

--- ast_parser.rb
+++ ast_parser.rb
@@ -745,7 +745,7 @@ class ASTParser
     string.
       delete_prefix('"').delete_suffix('"').
       force_encoding(Encoding::ASCII_8BIT).
-      gsub!(%r{\\\h{2}}) do |digits|
+      gsub(%r{\\\h{2}}) do |digits|
         digits.delete_prefix('\\').to_i(16).chr(Encoding::ASCII_8BIT)
       end.
       force_encoding(encoding)
