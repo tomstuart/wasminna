13656030fe7b5f9ba08a7730cbad0b7a99193daf
Tom Stuart <hi@tomstu.art>
2023-01-07 00:58:08 +0000

    Remove unused boilerplate from wasminna.rb
    The #main method and the `ARGV` check are no longer necessary; the
    former because there’s no other code left in the file, and the latter
    because nobody tries to require this file any more.

--- wasminna.rb
+++ wasminna.rb
@@ -2,13 +2,7 @@ require 'ast_parser'
 require 'interpreter'
 require 's_expression_parser'
 
-def main
-  unless ARGV.empty?
-    s_expression = SExpressionParser.new.parse(ARGF.read)
-    script = ASTParser.new.parse_script(s_expression)
-    Interpreter.new.evaluate_script(script)
-    puts
-  end
-end
-
-main
+s_expression = SExpressionParser.new.parse(ARGF.read)
+script = ASTParser.new.parse_script(s_expression)
+Interpreter.new.evaluate_script(script)
+puts
