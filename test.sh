a0e27a8d4ff216b0a91afc5bc672a92a9ef7b99a
Tom Stuart <hi@tomstu.art>
2023-01-07 01:02:53 +0000

    Rename wasminna.rb to run_wast.rb
    This file has had many purposes over its lifetime, but it’s now only
    used for one thing: executing the *.wast test files.

--- test.sh
+++ test.sh
@@ -19,14 +19,14 @@ for script in int_literals i32 i64 int_exprs float_literals conversions f32 f32_
 do
   file=$script.wast
   printf "\e[1m%s\e[0m: " "$file"
-  ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$file
+  ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/run_wast.rb "$WASM_SPEC_PATH"/test/core/$file
 done
 
 for pending in
 do
   file=$pending.wast
   printf "\e[1m%s\e[0m (pending): " "$file"
-  if ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/wasminna.rb "$WASM_SPEC_PATH"/test/core/$file; then
+  if ruby -I"$WASMINNA_PATH" "$WASMINNA_PATH"/run_wast.rb "$WASM_SPEC_PATH"/test/core/$file; then
     printf "\e[31merror: pending test passed\e[0m\n"
     exit 1
   else
