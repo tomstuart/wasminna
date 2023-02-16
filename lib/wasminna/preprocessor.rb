a65d7beb1d9de55f69363a386f5cd57fdff2d8ea
Tom Stuart <hi@tomstu.art>
2023-02-16 21:02:27 +0000

    Introduce trivial S-expression preprocessor
    This doesn’t do anything yet, but it gives us a place to start
    implementing desugaring (i.e. expanding abbreviations) before the
    S-expression reaches the AST parser.

---
+++ lib/wasminna/preprocessor.rb
@@ -0,0 +1,7 @@
+module Wasminna
+  class Preprocessor
+    def process_script(s_expression)
+      s_expression
+    end
+  end
+end
--- run_wast.rb
+++ run_wast.rb
@@ -1,8 +1,10 @@
 require 'wasminna/ast_parser'
 require 'wasminna/interpreter'
+require 'wasminna/preprocessor'
 require 'wasminna/s_expression_parser'
 
 s_expression = Wasminna::SExpressionParser.new.parse(ARGF.read)
-script = Wasminna::ASTParser.new.parse_script(s_expression)
+desugared_s_expression = Wasminna::Preprocessor.new.process_script(s_expression)
+script = Wasminna::ASTParser.new.parse_script(desugared_s_expression)
 Wasminna::Interpreter.new.evaluate_script(script)
 puts
