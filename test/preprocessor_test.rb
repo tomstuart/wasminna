6ff7ac1aa0316c4f56d71d7f03137d5fede7e715
Tom Stuart <hi@tomstu.art>
2023-02-17 01:37:06 +0000

    Add another unit test for expanding inline export abbreviations for multiple functions

--- test/preprocessor_test.rb
+++ test/preprocessor_test.rb
@@ -71,3 +71,24 @@ if actual == expected
 else
   raise actual.inspect unless actual == expected
 end
+
+input = [
+  ['module',
+    ['func', %w[export "i32.test"], %w[result i32], ['return', %w[i32.const 0x0bAdD00D]]],
+    ['func', %w[export "i32.umax"], %w[result i32], ['return', %w[i32.const 0xffffffff]]]
+  ]
+]
+expected = [
+  ['module',
+    [*%w[export "i32.test"], %w[func $__fresh_0]],
+    [*%w[func $__fresh_0], %w[result i32], ['return', %w[i32.const 0x0bAdD00D]]],
+    [*%w[export "i32.umax"], %w[func $__fresh_1]],
+    [*%w[func $__fresh_1], %w[result i32], ['return', %w[i32.const 0xffffffff]]]
+  ]
+]
+actual = Wasminna::Preprocessor.new.process_script(input)
+if actual == expected
+  print "\e[32m.\e[0m"
+else
+  raise actual.inspect unless actual == expected
+end
