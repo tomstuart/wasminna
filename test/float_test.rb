6def8bc34a5ad73cfe19991eafa82a363f0ac0c7
Tom Stuart <hi@tomstu.art>
2023-06-11 23:40:48 +0100

    Update float test assertions to use a different argument order
    This “input, then expected result” order is consistent with the other
    unit tests.

--- test/float_test.rb
+++ test/float_test.rb
@@ -1,13 +1,13 @@
-assert_float_encoding 0x4049_0fdb, 3141592653589793r / 10 ** 15, bits: 32
-assert_float_encoding 0x5eff_ffff, 0x7fffff4000000001r, bits: 32
-assert_float_encoding 0x0000_0000, 0r, bits: 32
-assert_float_encoding 0x4f00_0000, 0x7fffffffr, bits: 32
+assert_float_encoding 3141592653589793r / 10 ** 15, 0x4049_0fdb, bits: 32
+assert_float_encoding 0x7fffff4000000001r, 0x5eff_ffff, bits: 32
+assert_float_encoding 0r, 0x0000_0000, bits: 32
+assert_float_encoding 0x7fffffffr, 0x4f00_0000, bits: 32
 
-assert_float_encoding 0x3ff0_0000_0000_0000, 1r, bits: 64
-assert_float_encoding 0x4337_e2c4_4058_a799, 6723256985364377r, bits: 64
+assert_float_encoding 1r, 0x3ff0_0000_0000_0000, bits: 64
+assert_float_encoding 6723256985364377r, 0x4337_e2c4_4058_a799, bits: 64
 
-assert_float_decoding Wasminna::Float::Zero.new(sign: Wasminna::Sign::PLUS), 0x0000_0000, bits: 32
-assert_float_decoding Wasminna::Float::Finite.new(rational: 1r / 2 ** 149), 0x0000_0001, bits: 32
+assert_float_decoding 0x0000_0000, Wasminna::Float::Zero.new(sign: Wasminna::Sign::PLUS), bits: 32
+assert_float_decoding 0x0000_0001, Wasminna::Float::Finite.new(rational: 1r / 2 ** 149), bits: 32
 
 BEGIN {
   require 'wasminna/float'
@@ -16,9 +16,9 @@ BEGIN {
   require 'wasminna/float/zero'
   require 'wasminna/sign'
 
-  def assert_float_encoding(expected, rational, bits:)
+  def assert_float_encoding(input, expected, bits:)
     format = Wasminna::Float::Format.for(bits:)
-    actual = Wasminna::Float::Finite.new(rational:).encode(format:)
+    actual = Wasminna::Float::Finite.new(rational: input).encode(format:)
     if actual == expected
       print "\e[32m.\e[0m"
     else
@@ -26,9 +26,9 @@ BEGIN {
     end
   end
 
-  def assert_float_decoding(expected, encoded, bits:)
+  def assert_float_decoding(input, expected, bits:)
     format = Wasminna::Float::Format.for(bits:)
-    actual = Wasminna::Float.decode(encoded, format:)
+    actual = Wasminna::Float.decode(input, format:)
     if actual == expected
       print "\e[32m.\e[0m"
     else
