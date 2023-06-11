8dab130be48df2278bd4f2448875ba214bb38c40
Tom Stuart <hi@tomstu.art>
2023-06-11 23:40:48 +0100

    Rename float test assertion helpers
    These names are not only shorter, they also use the active verbs
    “encode” and “decode”, which is more consistent with the assertion
    helper names in other unit tests (e.g. “parse”, “preprocess”).

--- test/float_test.rb
+++ test/float_test.rb
@@ -1,13 +1,13 @@
-assert_float_encoding 3141592653589793r / 10 ** 15, 0x4049_0fdb, bits: 32
-assert_float_encoding 0x7fffff4000000001r, 0x5eff_ffff, bits: 32
-assert_float_encoding 0r, 0x0000_0000, bits: 32
-assert_float_encoding 0x7fffffffr, 0x4f00_0000, bits: 32
+assert_encode 3141592653589793r / 10 ** 15, 0x4049_0fdb, bits: 32
+assert_encode 0x7fffff4000000001r, 0x5eff_ffff, bits: 32
+assert_encode 0r, 0x0000_0000, bits: 32
+assert_encode 0x7fffffffr, 0x4f00_0000, bits: 32
 
-assert_float_encoding 1r, 0x3ff0_0000_0000_0000, bits: 64
-assert_float_encoding 6723256985364377r, 0x4337_e2c4_4058_a799, bits: 64
+assert_encode 1r, 0x3ff0_0000_0000_0000, bits: 64
+assert_encode 6723256985364377r, 0x4337_e2c4_4058_a799, bits: 64
 
-assert_float_decoding 0x0000_0000, Wasminna::Float::Zero.new(sign: Wasminna::Sign::PLUS), bits: 32
-assert_float_decoding 0x0000_0001, Wasminna::Float::Finite.new(rational: 1r / 2 ** 149), bits: 32
+assert_decode 0x0000_0000, Wasminna::Float::Zero.new(sign: Wasminna::Sign::PLUS), bits: 32
+assert_decode 0x0000_0001, Wasminna::Float::Finite.new(rational: 1r / 2 ** 149), bits: 32
 
 BEGIN {
   require 'wasminna/float'
@@ -16,7 +16,7 @@ BEGIN {
   require 'wasminna/float/zero'
   require 'wasminna/sign'
 
-  def assert_float_encoding(input, expected, bits:)
+  def assert_encode(input, expected, bits:)
     format = Wasminna::Float::Format.for(bits:)
     actual = Wasminna::Float::Finite.new(rational: input).encode(format:)
     if actual == expected
@@ -26,7 +26,7 @@ BEGIN {
     end
   end
 
-  def assert_float_decoding(input, expected, bits:)
+  def assert_decode(input, expected, bits:)
     format = Wasminna::Float::Format.for(bits:)
     actual = Wasminna::Float.decode(input, format:)
     if actual == expected
