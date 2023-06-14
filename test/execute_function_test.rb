5f6dfae4a4fd86ce65dd5c99f39d2e5a0e90402e
Tom Stuart <hi@tomstu.art>
2023-06-14 21:24:58 +0100

    Use consistent heredoc style in function execution tests
    This matches the preprocessor tests and is a little easier to read.

--- test/execute_function_test.rb
+++ test/execute_function_test.rb
@@ -1,91 +1,86 @@
-source =
-  <<~eos
-    (module
-      (func (export "add") (param i32 i32) (result i32)
-        (i32.add (local.get 0) (local.get 1))
-      )
+source = <<'--'
+  (module
+    (func (export "add") (param i32 i32) (result i32)
+      (i32.add (local.get 0) (local.get 1))
     )
-  eos
+  )
+--
 mod = Wasminna.load(source)
 raise unless mod.add(2, 3) == 5
 print "\e[32m.\e[0m"
 
-source =
-  <<~eos
-    (module
-      (func (export "dup") (param i32) (result i32 i32)
-        local.get 0
-        local.get 0
-      )
+source = <<'--'
+  (module
+    (func (export "dup") (param i32) (result i32 i32)
+      local.get 0
+      local.get 0
     )
-  eos
+  )
+--
 mod = Wasminna.load(source)
 raise unless mod.dup(6) == [6, 6]
 print "\e[32m.\e[0m"
 
-source =
-  <<~eos
-    (module
-      (func (export "add") (param f32 f32) (result f32)
-        (f32.add (local.get 0) (local.get 1))
-      )
+source = <<'--'
+  (module
+    (func (export "add") (param f32 f32) (result f32)
+      (f32.add (local.get 0) (local.get 1))
     )
-  eos
+  )
+--
 mod = Wasminna.load(source)
 raise unless mod.add(2.0, 3.0) == 5.0
 print "\e[32m.\e[0m"
 
-source =
-  <<~eos
-    (module
-      (func (export "add") (param i32 i32) (result i32)
-        (i32.add (local.get 0) (local.get 1))
-      )
+source = <<'--'
+  (module
+    (func (export "add") (param i32 i32) (result i32)
+      (i32.add (local.get 0) (local.get 1))
     )
-  eos
+  )
+--
 mod = Wasminna.load(source)
 raise unless mod.add(-2, 3) == 1
 print "\e[32m.\e[0m"
 
-source =
-  <<-eos
-    (func $fac (export "fac") (param i64) (result i64)
-      (if (result i64) (i64.eqz (local.get 0))
-        (then (i64.const 1))
-        (else
-          (i64.mul
-            (local.get 0)
-            (call $fac (i64.sub (local.get 0) (i64.const 1)))
-          )
+source = <<'--'
+  (func $fac (export "fac") (param i64) (result i64)
+    (if (result i64) (i64.eqz (local.get 0))
+      (then (i64.const 1))
+      (else
+        (i64.mul
+          (local.get 0)
+          (call $fac (i64.sub (local.get 0) (i64.const 1)))
         )
       )
     )
+  )
 
-    (func $fib (export "fib") (param i64) (result i64)
-      (if (result i64) (i64.le_u (local.get 0) (i64.const 1))
-        (then (i64.const 1))
-        (else
-          (i64.add
-            (call $fib (i64.sub (local.get 0) (i64.const 2)))
-            (call $fib (i64.sub (local.get 0) (i64.const 1)))
-          )
+  (func $fib (export "fib") (param i64) (result i64)
+    (if (result i64) (i64.le_u (local.get 0) (i64.const 1))
+      (then (i64.const 1))
+      (else
+        (i64.add
+          (call $fib (i64.sub (local.get 0) (i64.const 2)))
+          (call $fib (i64.sub (local.get 0) (i64.const 1)))
         )
       )
     )
+  )
 
-    (func $even (export "even") (param i64) (result i32)
-      (if (result i32) (i64.eqz (local.get 0))
-        (then (i32.const 44))
-        (else (call $odd (i64.sub (local.get 0) (i64.const 1))))
-      )
+  (func $even (export "even") (param i64) (result i32)
+    (if (result i32) (i64.eqz (local.get 0))
+      (then (i32.const 44))
+      (else (call $odd (i64.sub (local.get 0) (i64.const 1))))
     )
-    (func $odd (export "odd") (param i64) (result i32)
-      (if (result i32) (i64.eqz (local.get 0))
-        (then (i32.const 99))
-        (else (call $even (i64.sub (local.get 0) (i64.const 1))))
-      )
+  )
+  (func $odd (export "odd") (param i64) (result i32)
+    (if (result i32) (i64.eqz (local.get 0))
+      (then (i32.const 99))
+      (else (call $even (i64.sub (local.get 0) (i64.const 1))))
     )
-  eos
+  )
+--
 mod = Wasminna.load(source)
 raise unless 10.times.map { mod.fac(_1) } == [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
 raise unless 10.times.map { mod.fib(_1) } == [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
