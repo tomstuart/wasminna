343e053374566a4821b8bfec4b47a3a482e66821
Tom Stuart <hi@tomstu.art>
2023-06-14 20:14:51 +0100

    Quote `$file` everywhere in test.sh
    We’re doing this everywhere else to prevent globbing and word splitting
    [0] so we may as well do it here too.
    
    [0] https://www.shellcheck.net/wiki/SC2086

--- test.sh
+++ test.sh
@@ -19,14 +19,14 @@ done
 for file in "$WASM_SPEC_PATH"/test/core/*.wast
 do
   printf "\e[1m%s\e[0m: " "$(basename "$file")"
-  ruby -I"$WASMINNA_PATH"/lib "$WASMINNA_PATH"/run_wast.rb $file
+  ruby -I"$WASMINNA_PATH"/lib "$WASMINNA_PATH"/run_wast.rb "$file"
 done
 
 for pending in
 do
   file=$pending.wast
   printf "\e[1m%s\e[0m (pending): " "$(basename "$file")"
-  if ruby -I"$WASMINNA_PATH"/lib "$WASMINNA_PATH"/run_wast.rb "$WASM_SPEC_PATH"/test/core/$file; then
+  if ruby -I"$WASMINNA_PATH"/lib "$WASMINNA_PATH"/run_wast.rb "$WASM_SPEC_PATH"/test/core/"$file"; then
     printf "\e[31merror: pending test passed\e[0m\n"
     exit 1
   else
