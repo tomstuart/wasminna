b2e12162bba0c38a87ee867e3e4c87ea11379ddb
Tom Stuart <hi@tomstu.art>
2023-07-07 16:28:15 +0100

    Provide type definitions when processing expanded inline export

--- lib/wasminna/preprocessor.rb
+++ lib/wasminna/preprocessor.rb
@@ -265,11 +265,7 @@ module Wasminna
           [kind, id, *description]
         ]
 
-      read_list(from: expanded) { process_fields.call(DUMMY_TYPE_DEFINITIONS) }.then do |result|
-        after_all_fields do
-          result
-        end
-      end
+      read_list(from: expanded) { process_fields }
     end
 
     def process_typeuse
